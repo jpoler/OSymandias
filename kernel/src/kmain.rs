19c28581b512e6c1dd756414417be4945fdca2e0
Jon Poler <jonathan.poler@gmail.com>
2018-09-05 11:14:03 -0700

    timer + kmain

--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -8,16 +8,38 @@
 #![feature(attr_literals)]
 #![feature(never_type)]
 #![feature(ptr_internals)]
+// TODO remove me!
+#![feature(pointer_methods)]
 
 extern crate pi;
 extern crate stack_vec;
 
+pub mod console;
 pub mod lang_items;
 pub mod mutex;
-pub mod console;
 pub mod shell;
 
+use pi::timer::spin_sleep_ms;
+
+const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+
+const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
+const GFPSEL16: u32 = 0x00040000;
+const GPSET16: u32 = 0x00010000;
+const GPCLR16: u32 = 0x00010000;
+
 #[no_mangle]
-pub extern "C" fn kmain() {
-    // FIXME: Start the shell.
+pub unsafe extern "C" fn kmain() {
+    // STEP 1: Set GPIO Pin 16 as output.
+    GPIO_FSEL1.write_volatile(GFPSEL16);
+    // STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        GPIO_SET0.write_volatile(GPSET16);
+        spin_sleep_ms(5000);
+        GPIO_CLR0.write_volatile(GPCLR16);
+        spin_sleep_ms(5000);
+    }
 }
