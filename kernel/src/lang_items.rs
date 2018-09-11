786cd3e4c1c2198d44831604eea6953957b934d1
Jon Poler <jonathan.poler@gmail.com>
2018-09-11 00:00:37 -0700

    implement panic_fmt

--- kernel/src/lang_items.rs
+++ kernel/src/lang_items.rs
@@ -1,10 +1,29 @@
+use console::_print;
+
 #[no_mangle]
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
-pub extern fn panic_fmt(fmt: ::std::fmt::Arguments, file: &'static str, line: u32, col: u32) -> ! {
+pub extern "C" fn panic_fmt(
+    fmt: ::std::fmt::Arguments,
+    file: &'static str,
+    line: u32,
+    col: u32,
+) -> ! {
     // FIXME: Print `fmt`, `file`, and `line` to the console.
 
-    loop { unsafe { asm!("wfe") } }
+    kprint!(
+        "panic: {}\nfile: {}\nline: {}\ncol: {}\n",
+        fmt,
+        file,
+        line,
+        col
+    );
+
+    loop {
+        unsafe { asm!("wfe") }
+    }
 }
 
-#[cfg(not(test))] #[lang = "eh_personality"] pub extern fn eh_personality() {}
+#[cfg(not(test))]
+#[lang = "eh_personality"]
+pub extern "C" fn eh_personality() {}
